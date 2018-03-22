cd6df7c8193d43012404ad7d7458db8491c7b569
k.castro <k.castro14@gmail.com>
2018-03-22 18:31:21 -0400

    added dev_open and dev_close, made var/func static

--- Pa2.c
+++ Pa2.c
@@ -1,31 +1,54 @@
+/* 
+	Novaira Farnaz, Kenia Castro, Sandy Demian
+	Prog Assn #2 - Character-mode Linux driver
+	COP 4600, Spring 2018
+*/
+
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/cdev.h> //to register device to kernel
 #include <linux/semaphore.h>
 #include <linux/fs.h> //for open/close, read/write to device
 #include <asm/uaccess.h> //copy to user, from user (userSpace Kspace)
+
+MODULE_AUTHOR("Novaira Farnaz, Kenia Castro, Sandy Demian");
+MODULE_DESCRIPTION("Character-mode Linux driver as a kernal module");
+MODULE_LICENSE("GPL");
 
 struct examp_device{
 	char data[100];
 	struct semaphore sem;
 }virtual_device;
 
+// function prototypes
+static int dev_entry(void);
+static void dev_exit(void);
+static int dev_open(struct inode *inode, struct file *filp);
+static int dev_close(struct inode *inode, struct file *filp);
+static ssize_t dev_read(struct file *filp,	char *buffer, size_t length, loff_t * offset);
+static ssize_t dev_write(struct file *filp, const char *buffer, size_t length, loff_t * off);
+
 //global variables
-struct cdev *mcdev; //our character driver
-int major_number;
-int retv; //return values of kernel functions.
-dev_t dev_num;  //holds major number and minor number
 #define DEVICE_NAME "gerberDevice" //name of our device driver
 #define BUF_LEN 1024  // the buffer size
+
+struct cdev *mcdev; //our character driver
+static int major_number;
+int retv; //return values of kernel functions.
+dev_t dev_num;  //holds major number and minor number
+
 static char buf[BUF_LEN];	/* The msg the device will give when asked */
+static char *buf_Ptr;
 static int buf_index = 0;
+static int Dev_open = 0;
 
 struct file_operations fopi ={
 	.owner = THIS_MODULE,
-	//.open = dev_open,
-	//.release = dev_close,
-	//.write = dev_write,
-	//.read = dev_read
+	.open = dev_open,
+	.release = dev_close,
+	.write = dev_write,
+	.read = dev_read
 };
 
 //Initialization: registers a character device driver to the system
@@ -61,15 +84,34 @@ static void dev_exit(void) {
 	printk(KERN_ALERT "unloaded module");
 }
 
-static int dev_open() {
+// Open
+static int dev_open(struct inode *inode, struct file *filp) {
+	static int count = 0;
+	
+	if (Dev_open)
+		return -EBUSY;
+	
+	Dev_open++;
+	printk(KERN_INFO "Device has been opened %d times\n", count++);
+
+	buf_Ptr = buf;
+	try_module_get(THIS_MODULE);
+
+	return 0;
 }
 
-static int dev_close(){
+// Release
+static int dev_close(struct inode *inode, struct file *filp){
+	Dev_open--; // Ready for next caller
 	
+	printk(KERN_INFO "Goodbye!\n");
+	module_put(THIS_MODULE);
+	
+	return 0;
 }
 
 // read from the buffer
-ssize_t dev_read(struct file *filp,	char *buffer, size_t length, loff_t * offset){
+static ssize_t dev_read(struct file *filp,	char *buffer, size_t length, loff_t * offset){
 	// l - buffer - offset
 	int i = 0, j = 0, char_read;
 
